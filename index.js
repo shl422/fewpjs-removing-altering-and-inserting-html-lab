18bcd17701251635f286fcbd3cbef27e9404b6f2
shl422 <h.k.domingo@outlook.com>
2021-08-10 10:49:44 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "LUKKAAAA is the champion";
+// innerText = document.createTextNode("LUKAAAAA is the champion");
+// newHeader.appendChild(innerText);
+document.body.appendChild(newHeader);
